61d977fa39c33955617df1371663c35dab1e7305
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2026-06-17 20:49:28 +0700

    Refactor using cogs

--- cogs/basic.py
+++ cogs/basic.py
@@ -8,16 +8,6 @@ class Basic(commands.Cog):
     @commands.command()
     async def halo(self, ctx):
         await ctx.channel.send(f"Halo juga {ctx.author.mention}! ada yang bisa dibantu?")
-
-    @commands.command()
-    async def test(self, ctx):
-        embed = discord.Embed(
-        title="Informasi Bot", 
-        description="Ini adalah deskripsi embed.", 
-        color=discord.Color.blue()
-        )
-
-        await ctx.send(embed=embed)
-
+        
 async def setup(bot):
     await bot.add_cog(Basic(bot))
