3459338a49a975f0298d79e00ca01cc3290c4252
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2026-06-20 14:06:04 +0700

    iyah dikit

--- cogs/basic.py
+++ cogs/basic.py
@@ -30,12 +30,23 @@ class Basic(commands.Cog):
             color=user.color
         )
         
+        # variable timestamp
+        created_at = int(user.created_at.timestamp())
+        join_at = int(user.joined_at.timestamp())
+
         embed_profile.description = f" **Informasi Member {user.mention}**"
-        embed_profile.add_field(name="User ID", value=f"`{user.id}`")
+        embed_profile.add_field(name="User ID", value=f"`{user.id}`", inline=False)
         embed_profile.set_thumbnail(url=user.avatar.url)
 
         embed_profile.set_footer(text=f"{self.bot.user.name} © 2026", icon_url=self.bot.user.avatar.url)
+
+        embed_profile.add_field(name="Akun dibuat", value=f"<t:{created_at}:D>\n(<t:{created_at}:R>)")
         embed_profile.add_field(name="\u200b", value="\u200b", inline=True)
+        embed_profile.add_field(name="Bergabung ke server", value=f"<t:{join_at}:D>\n(<t:{join_at}:R>)")
+
+        # ambil semua role user
+        roles = [role.mention for role in user.roles if not role.is_default()]
+        embed_profile.add_field(name="Roles", value=f"{(', ').join(roles)}")
 
         await interaction.response.send_message(embed=embed_profile)
         
